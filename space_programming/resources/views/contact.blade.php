2511f9e1564055ddaef92883bb7bb742bd930ba9
Daan Pronk <daanpronk570@gmail.com>
2026-05-21 21:53:16 +0200

    02-Route Done

---
+++ space_programming/resources/views/contact.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    nav
+    <div>
+        <h3>GROUND STATION ADDRESS</h3>
+        <p>
+            Space Programming Headquarters<br>
+            Cosmic Code Operations Center<br>
+            Launch Complex 9, Sector F<br>
+            1024 Bitstream Way<br>
+            99101 Low Orbit District<br>
+            Terran Continental Zone
+        </p>
+
+        <div>
+            <div>
+                <h4>DOCKING REFERENCE</h4>
+                <p>Ground Station ID: GS-SP-09</p>
+            </div>
+            <div class=>
+                <h4>COORDINATES</h4>
+                <p>Lat 28.524° N · Long 80.648° W</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
